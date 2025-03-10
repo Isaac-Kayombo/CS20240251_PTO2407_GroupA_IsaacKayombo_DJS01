a17bf4b1e3d92cddd2dae20454bed332768aa0f8
IsaacK47 <isaackayombo41@gmail.com>
2025-03-10 20:34:22 +0200

    Function for converting kilometers to meters.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
  */
 
-// Given Parameters
+// GIVEN PARAMETERS
 const initialVelocity = 10000; // velocity (km/h)
 const acceleration = 3; // acceleration (m/s^2)
 const time = 3600; // seconds (1 hour)
@@ -13,8 +13,10 @@ const initialDistance = 0; // distance (km)
 const initialFuel = 5000; // remaining fuel (kg)
 const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
-
-
+// FUNCTION TO CONVERT KILOMETERS TO METERS
+const kmhToMs = (kmh) => {
+  return kmh * (1000 / 3600); // 1 km/h = 1000 meters / 3600 seconds
+}
 
 console.log(`Corrected New Velocity: ${} km/h`);
 console.log(`Corrected New Distance: ${} km`);
